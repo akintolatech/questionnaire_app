1f10b7e6a3ba94851d39bd62e0aad658c9466dae
akintolatechnologies <akintolatechnologies@gmail.com>
2025-03-30 00:07:36 +0100

    Questionnaire

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Teacher, Question, Response, QuestionCategory
+from .models import Teacher, Question, Response, QuestionCategory, Participant
 
 admin.site.register(Teacher)
 admin.site.register(Question)
 admin.site.register(Response)
 admin.site.register(QuestionCategory)
+admin.site.register(Participant)
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,34 +1,31 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Teacher, Question, Response, Participant, QuestionCategory
 
-
-
 def app_landing(request):
-    return render(
-        request,
-        "questionnaire/landing.html",
-    )
-
+    return render(request, "questionnaire/landing.html")
 
 def success(request):
-    return render(
-        request,
-        "questionnaire/success.html",
-    )
-
+    return render(request, "questionnaire/success.html")
 
 def questionnaire_view(request):
     teachers = Teacher.objects.all()
     questions = Question.objects.all()
-    categories = QuestionCategory.objects.all()  # Fixed: Should get categories, not questions
-    participants = Participant.objects.all()  # Fetch all participants
+    categories = QuestionCategory.objects.all()
+    participants = Participant.objects.all()
 
     if request.method == "POST":
         participant_name = request.POST.get("participant_name")
         participant_phone = request.POST.get("participant_phone")
 
         if participant_name and participant_phone:
-            participant, _ = Participant.objects.get_or_create(name=participant_name, phone_number=participant_phone)
+            participant, created = Participant.objects.get_or_create(
+                name=participant_name, phone_number=participant_phone
+            )
+
+            if not created:  # If participant already exists
+                messages.error(request, "A participant with this phone number already exists.")
+                return redirect("questionnaire")  # Redirect back to the form
 
             for teacher in teachers:
                 for question in questions:
@@ -36,12 +33,91 @@ def questionnaire_view(request):
                     response_value = request.POST.get(response_key)
 
                     if response_value:
-                        Response.objects.create(teacher=teacher, question=question, response=response_value)
-
-            return redirect("success")  # Redirect to success page
+                        Response.objects.create(
+                            participant=participant,  # Associate response with participant
+                            teacher=teacher,
+                            question=question,
+                            response=response_value
+                        )
+            messages.success(request, "Your responses have been successfully submitted.")
+            return redirect("success")  # Redirect on success
 
     return render(
         request,
         "questionnaire/questionnaire.html",
-        {"teachers": teachers, "questions": questions, "categories": categories, "participants": participants},
+        {
+            "teachers": teachers,
+            "questions": questions,
+            "categories": categories,
+            "participants": participants,
+        },
     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.shortcuts import render, redirect
+# from .models import Teacher, Question, Response, Participant, QuestionCategory
+#
+#
+#
+# def app_landing(request):
+#     return render(
+#         request,
+#         "questionnaire/landing.html",
+#     )
+#
+#
+# def success(request):
+#     return render(
+#         request,
+#         "questionnaire/success.html",
+#     )
+#
+#
+# def questionnaire_view(request):
+#     teachers = Teacher.objects.all()
+#     questions = Question.objects.all()
+#     categories = QuestionCategory.objects.all()  # Fixed: Should get categories, not questions
+#     participants = Participant.objects.all()  # Fetch all participants
+#
+#     if request.method == "POST":
+#         participant_name = request.POST.get("participant_name")
+#         participant_phone = request.POST.get("participant_phone")
+#
+#
+#         if participant_name and participant_phone:
+#             participant, _ = Participant.objects.get_or_create(name=participant_name, phone_number=participant_phone)
+#
+#             for teacher in teachers:
+#                 for question in questions:
+#                     response_key = f"response_{teacher.id}_{question.id}"
+#                     response_value = request.POST.get(response_key)
+#
+#                     if response_value:
+#                         Response.objects.create(teacher=teacher, question=question, response=response_value)
+#
+#             return redirect("success")  # Redirect to success page
+#
+#     return render(
+#         request,
+#         "questionnaire/questionnaire.html",
+#         {"teachers": teachers, "questions": questions, "categories": categories, "participants": participants},
+#     )
